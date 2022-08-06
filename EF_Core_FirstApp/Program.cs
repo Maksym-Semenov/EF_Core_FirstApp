7c9217750ee2a4ec9d1d83a007bd8acdb5931065
Maksym-Semenov <mestcomua@gmail.com>
2022-08-06 14:07:17 +0300

    Добавьте файлы проекта.

---
+++ EF_Core_FirstApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using EF_Core_FirstApp;
+using System;
+using System.Linq;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (helloappdbContext db = new helloappdbContext())
+            {
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.UserId}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EF_Core_FirstApp/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EF_Core_FirstApp
+{
+    public partial class User
+    {
+        public long UserId { get; set; }
+        public string Name { get; set; }
+        public long Age { get; set; }
+    }
+}
